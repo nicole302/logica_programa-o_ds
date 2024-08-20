af81b1682182e9bedd57a3f6ef6df3c73aa7d3ce
nicole302 <ncavalcantedutra@gmail.com>
2024-08-20 08:56:46 -0300

    meu primeiro projeto

---
+++ pasta/aula02.py
@@ -0,0 +1,20 @@
+# primeiro, reconhecemos as variasveis e adicionamos 
+nome = input  ('digite seu nome:')
+email = input ('dgite seu nome: ')
+telefone = input ('digite seu numero: ')
+
+gasolina = int(Input('Insira a gasolina gasta em km/l'))
+alcool = int(input('insira o alcool gato em km/l'))
+totalkm = 1280
+divi = 1280 / gasolina
+div2 = 1280 / alcool
+print (' a gasolina gasta é: {div: . 2f}km/l')
+print (' a alcool gasta é: {div2: . 2f}km/l')
+
+vlrgasolina = float(6.13)
+vlralcool = float(4.08)
+gastogasolina = div * vlrgasolina
+gastoalcool = div * vlralcool
+print (f 'o valor da gasolina gasto é: {gastogasolina:.2f}')
+print (f ' o valor do alcool gatso é: {gastoalcool:.2f}')
+''
